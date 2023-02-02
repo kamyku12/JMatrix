b711541d709d7c0a17f9793357f4b83791dd4d98
kamyku12 <70235972+kamyku12@users.noreply.github.com>
2023-02-02 12:31:57 +0100

    JavaDoc

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Matrix"},{"p":"<Unnamed>","l":"Shape"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DataFrame"},{"p":"<Unnamed>","l":"Matrix"},{"p":"<Unnamed>","l":"Shape"}];updateSearchResults();
